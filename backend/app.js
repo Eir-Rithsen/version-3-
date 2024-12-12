6eae74bcc63b7013a4c69dd55da6979d9dd058be
Ivan <68665931+Eir-Rithsen@users.noreply.github.com>
2024-12-12 01:00:49 -0300

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+// === Archivo: backend/app.js ===
+const express = require('express');
+const authRoutes = require('./routes/auth');
+const uploadRoutes = require('./routes/upload');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// Rutas
+app.use('/auth', authRoutes);
+app.use('/upload', uploadRoutes);
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
